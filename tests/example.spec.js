02ce7b8bed70bd4f06d5bf89efd223425d872e67
Jordan Cuadrado <jcuadrado@psecu.com>
2025-02-07 11:04:58 -0500

    testing

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,18 +2,38 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(process.env.BASE_URL || 'https://trunk-branching.web.app/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Trunk Branching/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('click on 1st button', async ({ page }) => {
+  await page.goto(process.env.BASE_URL || 'https://trunk-branching.web.app/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.locator("#click1").click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.locator("div").first()).toHaveText('Click 1');
+});
+
+test('click on 2nd button', async ({ page }) => {
+  await page.goto(process.env.BASE_URL || 'https://trunk-branching.web.app/');
+
+  // Click the get started link.
+  await page.locator("#click2").click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.locator("div").first()).toHaveText('Click 2');
+});
+
+test('click on 3rd button', async ({ page }) => {
+  await page.goto(process.env.BASE_URL || 'https://trunk-branching.web.app/');
+
+  // Click the get started link.
+  await page.locator("#click3").click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.locator("div").first()).toHaveText('Click 3');
 });
